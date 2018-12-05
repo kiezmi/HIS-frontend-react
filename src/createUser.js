220fd00f502cff797b6394d5474e33dfbf7f5338
Kiezmi Quinza <Kiezmi@gmail.com>
2018-12-05 16:56:30 +0100

    Cambion en los estilos

--- src/createUser.js
+++ src/createUser.js
@@ -1,69 +1,146 @@
-import React from 'react';
-import api from './services/api';
-import { Link } from 'react-router-dom';
+import React from "react";
+import api from "./services/api";
+import { Link } from "react-router-dom";
+import "./createUser.css";
 
 class CreateUser extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            role: 'patient',
-            uid: '',
-            name: '',
-            surname: '',
-            dni: '',
-            username: '',
-            password: ''
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      role: "patient",
+      uid: "",
+      name: "",
+      surname: "",
+      dni: "",
+      username: "",
+      password: ""
+    };
 
-        this.handleChange = this.handleChange.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    handleChange(ev) {
-        this.setState({
-            [ev.target.name]: ev.target.value
-        });
-    }
+  handleChange(ev) {
+    this.setState({
+      [ev.target.name]: ev.target.value
+    });
+  }
 
-    handleSubmit(ev) {
-        ev.preventDefault();
-        api.createUser(this.state);
-        this.cancelCourse();
-    }
+  handleSubmit(ev) {
+    ev.preventDefault();
+    api.createUser(this.state);
+    this.cancelCourse();
+  }
 
-    cancelCourse = () => {
-        document.getElementById("newuser").reset();
-    }
+  cancelCourse = () => {
+    document.getElementById("newuser").reset();
+  };
 
-    render() {
-        return (
-            <div>
-                <div className="userRegistry">
-                    <form id="newuser" onSubmit={this.handleSubmit.bind(this)}>
-                        <label>Rol: </label>
-                        <select name="role" value={this.state.value} onChange={this.handleChange}>>
-                            <option value="patient">Paciente</option>
-                            <option value="doctor">Doctor</option>
-                            <option value="technical">Técnico</option>
-                        </select>
-                        <label>ID: </label>
-                        <input type="text" name="uid" placeholder="introduzca id" value={this.state.newid} onChange={this.handleChange}></input><br />
-                        <label>Nombre: </label>
-                        <input type="text" name="name" placeholder="introduzca nombre" value={this.state.newname} onChange={this.handleChange}></input><br />
-                        <label>Apellidos: </label>
-                        <input type="text" name="surname" placeholder="introduzca apellidos" value={this.state.newsurname} onChange={this.handleChange}></input><br />
-                        <label>DNI: </label>
-                        <input type="text" name="dni" placeholder="introduzca DNI" value={this.state.newdni} onChange={this.handleChange}></input><br />
-                        <label>Nombre de usuario: </label>
-                        <input type="text" name="username" placeholder="introduzca nombre de usuario" value={this.state.newusername} onChange={this.handleChange}></input><br />
-                        <label>Contraseña: </label>
-                        <input type="text" name="password" placeholder="introduzca contraseña" value={this.state.newpass} onChange={this.handleChange}></input><br />
-                        <button type="submit">Registrar</button>
-                    </form>
-                    <Link to={'/'}><h4>Volver</h4></Link>
-                </div>
+  render() {
+    return (
+      <div className="userRegistry">
+        <div className="envolturaGrande">
+          <div className="contenedor">
+            <div className="cabecera">Registro</div>
+            <div className="cuerpo">
+              <form
+                className="formulario"
+                id="newuser"
+                onSubmit={this.handleSubmit.bind(this)}
+              >
+                <ul>
+                  <li>
+                    <label>Rol: </label>
+                    <select
+                      required={true}
+                      name="role"
+                      value={this.state.value}
+                      onChange={this.handleChange}
+                    >
+                      <option value="undefined" />
+                      <option value="patient">Paciente</option>
+                      <option value="doctor">Doctor</option>
+                      <option value="technical">Técnico</option>
+                    </select>
+                  </li>
+                  {/* <li>
+                    <label>ID: </label>
+                    <input
+                      type="text"
+                      name="uid"
+                      placeholder="introduzca id"
+                      value={this.state.newid}
+                      onChange={this.handleChange}
+                    />
+                  </li> */}
+                  <li>
+                    <label>Nombre: </label>
+                    <input
+                      required={true}
+                      type="text"
+                      name="name"
+                      placeholder="introduzca nombre"
+                      value={this.state.newname}
+                      onChange={this.handleChange}
+                    />
+                  </li>
+                  <li>
+                    <label>Apellidos: </label>
+                    <input
+                      required={true}
+                      type="text"
+                      name="surname"
+                      placeholder="introduzca apellidos"
+                      value={this.state.newsurname}
+                      onChange={this.handleChange}
+                    />
+                  </li>
+                  <li>
+                    <label>DNI: </label>
+                    <input
+                      required={true}
+                      type="text"
+                      name="dni"
+                      placeholder="introduzca DNI"
+                      value={this.state.newdni}
+                      onChange={this.handleChange}
+                    />
+                  </li>
+                  <li>
+                    <label>Nombre de usuario: </label>
+                    <input
+                      required={true}
+                      type="text"
+                      name="username"
+                      placeholder="introduzca nombre de usuario"
+                      value={this.state.newusername}
+                      onChange={this.handleChange}
+                    />
+                  </li>
+                  <li>
+                    <label>Contraseña: </label>
+                    <input
+                      required={true}
+                      type="text"
+                      name="password"
+                      placeholder="introduzca contraseña"
+                      value={this.state.newpass}
+                      onChange={this.handleChange}
+                    />
+                  </li>
+                </ul>
+                <button className="submit" type="submit">
+                  Registrar
+                </button>
+              </form>
             </div>
-        )
-    }
+            <div className="footer">
+              <Link to={"/"}>Volver</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CreateUser;
